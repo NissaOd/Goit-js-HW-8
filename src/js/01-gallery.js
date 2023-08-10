40e740e8fd0f944911e922fbf75381eb2085b803
Nissa <89179920+NissaOd@users.noreply.github.com>
2023-08-10 14:56:54 +0300

    +

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,29 +1,24 @@
 import SimpleLightbox from 'simplelightbox';
 import "simplelightbox/dist/simple-lightbox.min.css";
-// Add imports above this line
+// Add imports above this line +
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+// Change code below this line+
 
 const gallery = document.querySelector('.gallery');
 
 const galleryMarkup = galleryItems
-  .map(
-    ({ preview, original, description }) =>
-      `<a class="gallery__item" href="${original}">
+    .map(
+        ({ preview, original, description }) =>
+        `<a class="gallery__item" href="${original}">
       <img class="gallery__image" src="${preview}" alt="${description}"/>
       </a>`
-  )
-  .join('');
+    )
+    .join('');
 
 gallery.insertAdjacentHTML('afterbegin', galleryMarkup);
 
 new SimpleLightbox('.gallery a', {
-  captionsData: 'alt',
-  captionPosition: 'bottom',
-  captionDelay: 250,
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
 });
-
-
-
-
-
